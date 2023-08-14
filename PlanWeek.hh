9d2bbcca7962f8b63eb9be740ab785e3d8fdf939
Rob Kutschke <kutschke@fnal.gov>
2023-08-13 21:38:33 -0500

    Add data member to hold information about data volume. Not yet filled.

--- PlanWeek.hh
+++ PlanWeek.hh
@@ -1,6 +1,8 @@
 #ifndef PlanWeek_hh
 #define PlanWeek_hh
 
+#include "Data.hh"
+
 #include "TDatime.h"
 
 #include <iosfwd>
@@ -14,6 +16,8 @@ public:
   TDatime t0()   const { return _t0;  }
   TDatime tend() const { return _tend;}
 
+  std::map<DataType,Data> const& data() const { return _data;}
+
   unsigned nDays()          const { return _ndays;     }
   unsigned duration()       const { return _duration;  }
   float    durationInDays() const { return _durationInDays; }
@@ -21,15 +25,20 @@ public:
   bool     springDST()      const { return _springDST; }
   bool     fallDST()        const { return _fallDST;   }
 
+
+
 private:
-  TDatime _t0;                  // Date at start of week, Sunday 00:00:00
-  TDatime _tend;                // Date at end of week, Saturday 23:59:59
-  unsigned _duration = 0;       // Duration of the week, in seconds ( DST Transistions are shorter or longer )
-  float    _durationInDays = 0; // _duration in units of floating point days.
-  float    _fraction = 0;       // Fraction of the week within the plan; only different from 1 on first and last week.
-  unsigned _ndays = 0;          // Number of days in the week; most weeks will be 7.  The first and last weeks may be truncated.
-  bool     _springDST = false;  // This week contains a spring forward daylight savings transition
-  bool     _fallDST   = false;  // This week contains a fall back daylight savings transition
+  TDatime _t0;                   // Date at start of week, Sunday 00:00:00
+  TDatime _tend;                 // Date at end of week, Saturday 23:59:59
+
+  std::map<DataType,Data> _data; // Amount of data taken this week.
+
+  unsigned _duration = 0;        // Duration of the week, in seconds ( DST Transistions are shorter or longer )
+  float    _durationInDays = 0;  // _duration in units of floating point days.
+  float    _fraction = 0;        // Fraction of the week within the plan; only different from 1 on first and last week.
+  unsigned _ndays = 0;           // Number of days in the week; most weeks will be 7.  The first and last weeks may be truncated.
+  bool     _springDST = false;   // This week contains a spring forward daylight savings transition
+  bool     _fallDST   = false;   // This week contains a fall back daylight savings transition
 
   // Compute the date at the end of this week.
   TDatime endOfWeek_with_DST_adjustment( TDatime const& t0 );
