4c94eb92e5160f68656f4e6450003aad50d46ec6
Rob Kutschke <kutschke@fnal.gov>
2023-08-22 17:11:40 -0500

    Exersize BB2 and sumYear.

--- main.cc
+++ main.cc
@@ -1,6 +1,10 @@
+#include "BB1.hh"
+#include "BB2.hh"
 #include "PlanInputs.hh"
 #include "Plan.hh"
 #include "PlotMaker.hh"
+
+#include "sumYear.hh"
 
 #include <iostream>
 
@@ -23,6 +27,13 @@ int main( int nargs, char** argc) {
   // Make the plots etc
   PlotMaker pm( inp, plan);
   pm.plotRawData();
+
+  // Under development.
+  BB2 bb2;
+  std::cout << "BB2 status: " << bb2.status() << std::endl;
+
+  // Cross-check that sums over a typical year make sense
+  sumYear( inp, plan, 2030);
 
   return 0;
 }
