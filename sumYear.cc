6aa0c2f879f3a075b1f00220994d673c262e7b38
Rob Kutschke <kutschke@fnal.gov>
2023-08-25 15:00:26 -0500

    Post development cleanup.

--- sumYear.cc
+++ sumYear.cc
@@ -23,48 +23,20 @@ void sumYear( PlanInputs const& inputs, Plan const& plan, int year, DataType::en
         auto i = res.rawData.find( DataType(dtype) );
         if ( i != res.rawData.end() ){
           Data const& data = i->second;
-          /*
-          cout << "2030 Week:  "
-             << wi.nDays() << " "
-             << std::setw(10) <<  wi.fractionOfWeek() << " "
-             << res.rawData.size() <<  " "
-             << wi.week().t0().AsString() << " "
-             << std::setw(7) << sumDays << " "
-             << std::setw(10) << sumFraction
-             << endl;
-          */
-          double f = 1.;
-          //cout << "       sdata:  " << i->first << "  " << data << "   |  " << wi.nDays() << endl;
-          if ( wi.nDays() !=7 && data.nDays() != 7 ){
-            cerr << "Weird: both data and week are fractional.  Might be Ok but need to check. "
-                 << "\nNumber of days in week: " << wi.nDays() << "  Number of days in data: " << data.nDays() << endl;
-            throw std::logic_error("A case that I did not think could happen.");
-          }
-          f = wi.fractionOfWeek();
+          double f = wi.fractionOfWeek();
           double events = data.events();
           double size   = data.size();
           int    nDays  = data.nDays();
-          if ( wi.nDays() != 7 && data.nDays() == 7){
+          if ( wi.nDays() != 7 ){
             events *= f;
             size   *= f;
             nDays   = std::round( double(nDays)*f);
-          } else if ( wi.nDays() ==7 && data.nDays() !=7 ){
-            cout  << "Mark short week: " << endl;
-            f = double(nDays)/7.;
           }
           sumData      += size;
           sumEvents    += events;
           sumFraction  += f;
           sumDays      += nDays;
-
         }
-        /*
-        cout << "        Sums:  "
-             << std::setw(7) << sumDays << " "
-             << std::setw(10) << sumFraction
-             << endl;
-        */
-
       }
     }
   }
